be070f3f2de9e01e2c4eb1e61083092377f8a7ec
Sanath S <sanath@amazeinc.in>
2024-04-03 10:39:13 +0530

    added timeout

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  //await app.listen(3000);
+  const server = await app.listen(3000);
+  server.setTimeout(1800000);
 }
 bootstrap();
