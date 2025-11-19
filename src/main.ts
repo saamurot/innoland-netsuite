45873914f64f165d0394f8af3db8601653bb7a54
Sanath S <sanath@amazeinc.in>
2025-11-19 10:28:49 +0530

    updated main file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  //await app.listen(3000);
   const server = await app.listen(3000);
   server.setTimeout(600000);
 }
